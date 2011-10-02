a1af697651541ad7b6b13d443941ef88f96e9ec9
Shyam Ramaswamy <sshyamr@ncsu.edu>
2011-10-01 20:05:12 -0400

    Added Edit user functionality

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,6 +15,22 @@ module SessionsHelper
   end
 
 
+  def deny_access
+    store_location
+    flash[:notice] = "Please Sign in to access this page."
+    redirect_to signin_path
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+
   def current_user=(user)
     @current_user = user
   end
@@ -31,6 +47,14 @@ module SessionsHelper
 
     def remember_token
       cookies.signed[:remember_token] || [nil, nil]
+    end
+
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
     end
 
 end
